4f6f75ba8652256f77130d75e8d8f26ed3fca341
frntEndDvlpr <reactdvlpr@gmail.com>
2025-01-17 17:10:41 +0400

    Database Installation

--- AttendanceBackend/settings.py
+++ AttendanceBackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'AttendanceBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'attendance',
+        'USER': 'postgres',
+        'PASSWORD': '123456asdfgh',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
