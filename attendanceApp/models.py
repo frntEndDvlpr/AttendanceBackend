2ca19bce5fed9798f2fa80170d3341c46a7cb650
frntEndDvlpr <reactdvlpr@gmail.com>
2025-01-17 19:17:21 +0400

    update and migrate attendance model

--- attendanceApp/models.py
+++ attendanceApp/models.py
@@ -3,14 +3,11 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
-    email = models.EmailField()
-    phone = models.CharField(max_length=10)
-    designation = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    date_of_joining = models.DateField()
-    date_of_birth = models.DateField()
-    address = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=10, blank=True, null=True)
+    designation = models.CharField(max_length=100, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+    date_of_joining = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -23,23 +20,31 @@ class PhotoLibrary(models.Model):
         return self.employee.name
     
 class AttendanceLog(models.Model):
+    STATUS_CHOICES = [
+        ('present', 'Present'),
+        ('absent', 'Absent'),
+        ('weekend', 'Weekend'),
+        ('public_holiday', 'Public Holiday'),
+    ]
+
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     date = models.DateField()
     time_in = models.TimeField()
     time_out = models.TimeField()
-    status = models.CharField(max_length=10)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, blank=True, null=True)
     photo = models.ImageField(upload_to='images/')
     location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.employee.name
         
-class project(models.Model):
+class Project(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     employees = models.ManyToManyField(Employee)
+    location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
