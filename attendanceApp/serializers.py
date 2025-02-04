62c1c3c983c3c3bf1dea89bdce935575b076dc72
frntEndDvlpr <reactdvlpr@gmail.com>
2025-02-04 07:40:24 +0400

    maintain

--- attendanceApp/serializers.py
+++ attendanceApp/serializers.py
@@ -3,21 +3,25 @@ from .models import Employee, PhotoLibrary, AttendanceLog, Project
 
 
 class PhotoLibrarySerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = PhotoLibrary
         fields = ['id', 'image', 'employee']
 
 class AttendanceLogSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = AttendanceLog
         fields = ['id', 'employee', 'date', 'time_in', 'time_out', 'status', 'photo', 'location']
 class ProjectSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Project
         fields = ['id', 'title', 'description', 'start_date', 'end_date', 'client', 'attendanceRange', 'location',]
         
 class EmployeeSerializer(serializers.ModelSerializer):
     projects=ProjectSerializer(many=True)
+
     class Meta:
         model = Employee
         fields = ['id', 'name', 'employeeCode', 'email', 'phone', 'designation', 'department', 'projects', 'date_of_joining']
