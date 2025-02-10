dc8deb030441384b6e2bc5fc158da19db94ab50f
frntEndDvlpr <reactdvlpr@gmail.com>
2025-02-10 12:24:22 +0400

    maintain header request

--- AttendanceBackend/settings.py
+++ AttendanceBackend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,4 +149,5 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
