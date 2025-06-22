b06ac6dd512737f4ac29ab819f46f2a4d8705211
Abbas <reactdvlpr@gmail.com>
2025-06-22 13:02:38 +0400

    exporting "is_staff" prop to the frontend DONE

--- AttendanceBackend/settings.py
+++ AttendanceBackend/settings.py
@@ -163,5 +163,7 @@ SIMPLE_JWT = {
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'user': 'core.serializers.UserSerializer',
+        "current_user": "core.serializers.UserSerializer",
     }
 }
--- core/serializers.py
+++ core/serializers.py
@@ -1,11 +1,17 @@
+from djoser.serializers import UserSerializer as BaseUserSerializer
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from rest_framework import serializers
-
 from attendanceApp.models import Employee
-
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     employee = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), required=False)
 
-    class Meta (BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username','email', 'password', 'employee']
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'email', 'password', 'employee']
+
+class UserSerializer(BaseUserSerializer):
+    employee = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), required=False)
+    is_staff = serializers.BooleanField(read_only=True)  # ✅ Safe and correct
+
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'is_staff', 'employee']
