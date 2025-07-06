0e315666623bb4b7eaa964806492cb3dbab54acd
Abbas <reactdvlpr@gmail.com>
2025-07-06 19:10:36 +0400

    prepairing to deyploy

--- AttendanceBackend/settings/prod.py
+++ AttendanceBackend/settings/prod.py
@@ -1,21 +1,15 @@
 import os
+import dj_database_url
 from .common import *
 
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['attendance-ksa-247f689c4e51.herokuapp.com']
 CORS_ALLOWED_ORIGINS = ['*']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'attendance',
-        'USER': 'postgres',
-        'PASSWORD': '123456asdfgh',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config()
 }
