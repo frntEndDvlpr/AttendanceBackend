b0abd4a09d435a3eaece622e2c798786e669305b
Abbas <reactdvlpr@gmail.com>
2025-07-06 23:32:27 +0400

    edit common file

--- AttendanceBackend/settings/prod.py
+++ AttendanceBackend/settings/prod.py
@@ -5,8 +5,9 @@ from .common import *
 
 DEBUG = False
 
+CORS_ALLOW_ALL_ORIGINS = False
 ALLOWED_HOSTS = ['attendance-ksa-247f689c4e51.herokuapp.com']
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = ['https://attendance-ksa-247f689c4e51.herokuapp.com',]
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
