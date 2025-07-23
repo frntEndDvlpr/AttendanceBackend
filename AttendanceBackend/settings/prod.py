d9ce1d783e04c8fe38a6390a02238a95a7fed0bd
Abbas <reactdvlpr@gmail.com>
2025-07-23 16:38:16 +0400

    add ksa attendance link

--- AttendanceBackend/settings/prod.py
+++ AttendanceBackend/settings/prod.py
@@ -7,7 +7,10 @@ DEBUG = False
 
 CORS_ALLOW_ALL_ORIGINS = False
 ALLOWED_HOSTS = ['attendance-ksa-247f689c4e51.herokuapp.com']
-CORS_ALLOWED_ORIGINS = ['https://attendance-ksa-247f689c4e51.herokuapp.com',]
+CORS_ALLOWED_ORIGINS = [
+    'https://attendance-ksa-247f689c4e51.herokuapp.com',
+    'http://64.23.176.226:8069/web#action=714&model=attendance.api.config&view_type=list&cids=1&menu_id=429',
+]
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
